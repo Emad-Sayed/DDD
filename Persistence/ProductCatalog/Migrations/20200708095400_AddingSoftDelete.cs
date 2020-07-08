0c03fa128cf4f188994694071040260eb4351375
Mohamed Elsherbiny <melsherbiny@brimo.co>
2020-07-08 11:55:13 +0200

    Adding soft delete migrations

---
+++ Persistence/ProductCatalog/Migrations/20200708095400_AddingSoftDelete.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.ProductCatalog.Migrations
+{
+    public partial class AddingSoftDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Units",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Units");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+        }
+    }
+}
